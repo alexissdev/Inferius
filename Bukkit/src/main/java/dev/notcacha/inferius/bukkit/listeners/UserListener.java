859bed354a92c3079bfb308a7bcc4272b4461dc3
cassha <notcacha1998@gmail.com>
2020-10-16 05:20:28 +0200

    Fixed UserListener

--- Bukkit/src/main/java/dev/notcacha/inferius/bukkit/listeners/UserListener.java
+++ Bukkit/src/main/java/dev/notcacha/inferius/bukkit/listeners/UserListener.java
@@ -3,6 +3,7 @@ package dev.notcacha.inferius.bukkit.listeners;
 import com.google.inject.Inject;
 import dev.notcacha.inferius.bukkit.user.User;
 import dev.notcacha.inferius.cache.Cache;
+import dev.notcacha.inferius.model.load.ModelLoad;
 import dev.notcacha.inferius.storage.Storage;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,24 +16,20 @@ public class UserListeners implements Listener {
 
     @Inject
     private Storage<User> userStorage;
-
     @Inject
     private Cache<UUID, User> userCache;
+    @Inject
+    private ModelLoad<User> userLoad;
 
     @EventHandler
     public void onAsyncPreLogin(AsyncPlayerPreLoginEvent event) {
-//        userStorage.load(event.getUniqueId().toString());
+        userLoad.loadAsync(event.getUniqueId().toString());
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         UUID id = event.getPlayer().getUniqueId();
 
-        userStorage.saveAsync(
-                userCache.find(id)
-                        .orElseThrow(() -> new IllegalArgumentException(
-                                "An error occurred while trying to save the user {id=" + id.toString() + "}")
-                        )
-        );
+        userCache.find(id).ifPresent(user -> userStorage.saveAsync(user));
     }
 }
